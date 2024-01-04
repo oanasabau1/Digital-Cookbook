974f25c128f88c39d898cbcacbf1e2a850f5e592
Oana Sabău <115418520+oanasabau1@users.noreply.github.com>
2024-01-04 21:10:58 +0200

    Update recipeRoutes.js

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -3,10 +3,6 @@ const router = express.Router();
 const Category = require("../models/Category");
 const Recipe = require("../models/Recipe");
 
-/**
- * These were the routes before implementing Command pattern
- */
-
 const recipeController = require("../controllers/recipeController");
 
 /**
@@ -25,43 +21,5 @@ router.get("/about", recipeController.about);
 router.delete('/recipe/recipe-delete/:_id', recipeController.deleteRecipe);
 router.get('/recipe/:_id/edit', recipeController.editRecipe);
 router.post('/recipe/:_id/update', recipeController.updateRecipe);
-
-
-
-
-// const categoryInstance = new Category();
-// const recipeInstance = new Recipe();
-
-
-// const HomepageCommand = require("../controllers/HomepageCommand");
-// const homepageCommandInstance = new HomepageCommand(categoryInstance, recipeInstance);
-// router.get("/", async (req, res) => {
-//   try {
-//     await homepageCommandInstance.execute(req, res, categoryInstance, recipeInstance);
-//   } catch (error) {
-//     res.status(500).send({ message: "Internal server error" });
-//   }
-// });
-
-// const ExploreCategoriesCommand = require("../controllers/exploreCategoriesCommand");
-// const exploreCategoriesCommandInstance = new ExploreCategoriesCommand(categoryInstance);
-// router.get("/categories", async (req, res) => {
-//   try {
-//     await exploreCategoriesCommandInstance.execute(req, res, categoryInstance);
-//   } catch (error) {
-//     res.status(500).send({ message: "Internal server error" });
-//   }
-// });
-
-
-// const ExploreCategoriesByIdCommand = require("../controllers/ExploreCategoriesByIdCommand");
-// const exploreCategoriesByIdCommandInstance = new ExploreCategoriesByIdCommand(recipeInstance);
-// router.get("/categories/:_id", async (req, res) => {
-//   try {
-//     await exploreCategoriesByIdCommandInstance.execute(req, res, recipeInstance);
-//   } catch (error) {
-//     res.status(500).send({ message: "Internal server error" });
-//   }
-// });
 
 module.exports=router;
